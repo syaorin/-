3a1d4b9236366b710def39a1ca534a13110039a5
syaorin <64355738+syaorin@users.noreply.github.com>
2020-07-15 14:50:43 +0900

    Add files via upload

---
+++ kadai_saikou/fusenn.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>付箋</title>
+</head>
+<body>
+    <textarea name="付箋" id="fusenn" cols="30" rows="10"></textarea>
+    <button>保存</button>
+</body>
+</html>
